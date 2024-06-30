68461dd745ef2b7aee4b3d0e5e4ef1e56fe11dc9
amjad-salameh <amjadsalameh00@gmail.com>
2024-06-30 10:21:14 +0300

    add the first page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import "./index.css";
 import LoginForm from "./Components/LoginForm/LoginForm";
+import FirstPage from "./Components/FirstPage/FirstPage";
+import { Route, Routes,Link } from "react-router-dom";
 
 function App() {
-  return <>
-  
-<LoginForm/>
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<FirstPage />} />
 
-
-
-
-
-
-  </>;
+        <Route path="/loginform" element={<LoginForm />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
